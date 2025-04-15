589645a200c122aa923654f4542c79a002318f9d
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-04-16 01:31:14 +0600

    Project3 commit

---
+++ project3.java
@@ -0,0 +1,9 @@
+public class project3{
+    public static void main(String[] args) {
+        
+     System.out.println("I AM FROME BANGLADESH");
+     System.out.println("i was bron in noakhali");
+    
+    }
+    
+}
